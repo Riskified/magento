367e0686477d360abca86d3900ef0015e42b1a66
assaf <assaf@riskified.com>
2013-04-05 11:49:26 +0300

    rename actions to Submit to Riksified, to differentiate from the new Riskified button

--- app/code/local/Riskified/Full/Block/Adminhtml/View.php
+++ app/code/local/Riskified/Full/Block/Adminhtml/View.php
@@ -6,7 +6,7 @@ class Riskified_Full_Block_Adminhtml_View extends Mage_Adminhtml_Block_Sales_Ord
 		parent::__construct();
 		$message = Mage::helper('sales')->__('Are you sure you want to submit this order to Riskified ?');
 		$this->_addButton('riski_submit', array(
-				'label'     => Mage::helper('sales')->__('Riskified'),
+				'label'     => Mage::helper('sales')->__('Submit to Riskified'),
 				'onclick'   => 'deleteConfirm(\''.$message.'\', \'' . $this->getRiskiUrl() . '\')',
 		));
 	}
