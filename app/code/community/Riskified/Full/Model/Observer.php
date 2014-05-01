f418521f2b7b6e0d63ed581122c6d3daaade2207
dormin <nimrod@riskified.com>
2014-05-01 23:18:24 +0300

    debugging authorizenet

--- app/code/community/Riskified/Full/Model/Observer.php
+++ app/code/community/Riskified/Full/Model/Observer.php
@@ -127,6 +127,8 @@ class Riskified_Full_Model_Observer{
     private function getCustomer($model) {
         $customer_id = $model->getCustomerId();
         $customer_details = Mage::getModel('customer/customer')->load($customer_id);
+        Mage::log('getCustomer $customer_details: ' . serialize($customer_details));
+
         $customer_order_details = Mage::getModel('sales/order')->getCollection()
             ->addFieldToFilter('customer_id', array('eq' => $customer_id))
             ->addFieldToSelect('entity_id')
@@ -174,15 +176,12 @@ class Riskified_Full_Model_Observer{
 
         switch ($payment->getMethod()) {
             case 'authorizenet':
-                Mage::log('authorizenet additionalInformation: ' . serialize($payment->getAdditionalInformation()));
-                foreach ($payment->getAdditionalInformation() as $additional_data){
-                    foreach ($additional_data as $key => $trans_data) {
-                        $avs_result_code = $trans_data['cc_avs_result_code'];
-                        $cvv_result_code = $trans_data['cc_response_code'];
-                        $credit_card_number  = "XXXX-XXXX-".$trans_data['cc_last4'];
-                        $credit_card_company = $trans_data['cc_type'];
-                    }
-                }
+                $card_data = array_values($payment->getAdditionalInformation('authorize_cards'))[0];
+                Mage::log('authorizenet card_data: ' . serialize($card_data);
+                $avs_result_code = $card_data['cc_avs_result_code'];
+                $cvv_result_code = $card_data['cc_response_code'];
+                $credit_card_number  = "XXXX-XXXX-".$card_data['cc_last4'];
+                $credit_card_company = $card_data['cc_type'];
                 break;
 
             case 'paypal_direct':
