c76de414cc4a0005a9f530f5ffb47492e8ecdd9a
Dan Fugina <dan@ablecanyon.com>
2014-07-29 20:53:09 -0400

    Fixing fatal error in Adminhtml/FullController.php

--- app/code/community/Riskified/Full/controllers/Adminhtml/FullController.php
+++ app/code/community/Riskified/Full/controllers/Adminhtml/FullController.php
@@ -6,10 +6,9 @@ class Riskified_Full_Adminhtml_FullController extends Mage_Adminhtml_Controller_
     public function riskiAction()
     {
         $id = $this->getRequest()->getParam('order_id');
-        $call = Mage::getModel('Riskified_Full_Model_Observer');
+        $call = Mage::getModel('full/observer');
         $call->postOrderIds(array($id));
-        Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
-        Mage::logException($e);
+
         Mage::app()->getResponse()->setRedirect(Mage::helper('adminhtml')->getUrl("adminhtml/sales_order/index", array('id'=>$id)));
     }
 
