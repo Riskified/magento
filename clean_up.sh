dc3ab49edb32741d2515f0a45abaeae574c270c6
Assaf Feldman <assaf@riskified.com>
2014-07-31 19:50:55 +0300

    Delete clean_up.sh

--- clean_up.sh
+++
@@ -1,8 +0,0 @@
-rm -rf /opt/bitnami/apps/magento/htdocs/js/riskified/
-rm -rf /opt/bitnami/apps/magento/htdocs/app/code/community/Riskified/
-rm -rf /opt/bitnami/apps/magento/htdocs/app/code/local/Riskified/
-rm /opt/bitnami/apps/magento/htdocs/app/etc/modules/Riskified_Full.xml
-rm /opt/bitnami/apps/magento/htdocs/app/design/adminhtml/default/default/layout/full.xml
-rm /opt/bitnami/apps/magento/htdocs/app/design/adminhtml/default/default/template/full/jsinit.phtml
-rm /opt/bitnami/apps/magento/htdocs/app/design/frontend/default/default/layout/full.xml
-rm /opt/bitnami/apps/magento/htdocs/skin/adminhtml/default/default/images/riskified/logo.jpg
