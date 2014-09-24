e2345bfaa9fee87ad5d51fbd04fd39b2984e0ba4
dormin <nimrod@riskified.com>
2014-09-24 23:49:25 +0300

    replace [] array init with array() syntax for backward compatibility

--- lib/riskified_php_sdk/src/Riskified/OrderWebhook/Transport/AbstractTransport.php
+++ lib/riskified_php_sdk/src/Riskified/OrderWebhook/Transport/AbstractTransport.php
@@ -129,9 +129,8 @@ abstract class AbstractTransport {
      * @return string
      */
     protected function endpoint_prefix() {
-//        $protocol = ($this->use_https) ? 'https' : 'http';
-//        return "$protocol://$this->url/api/";
-        return "http://s.riskified.com/api/";
+        $protocol = ($this->use_https) ? 'https' : 'http';
+        return "$protocol://$this->url/api/";
     }
 
     /**
