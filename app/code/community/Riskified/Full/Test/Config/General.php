f4a488e6610af99f1ec69ae201cb7146aec1aede
Dan Fugina <dan@ablecanyon.com>
2014-09-26 11:36:05 +0300

    Implementing some example tests

---
+++ app/code/community/Riskified/Full/Test/Config/General.php
@@ -0,0 +1,61 @@
+<?php
+
+class Riskified_Full_Test_Config_General extends EcomDev_PHPUnit_Test_Case_Config
+{
+
+    /**
+     * Make sure all of our rewrites are in effect
+     *
+     * @test
+     */
+    public function testRewrites()
+    {
+        $this->assertBlockAlias('adminhtml/sales_order_grid', 'Riskified_Full_Block_Adminhtml_Grid');
+
+        $this->assertModelAlias('paygate/authorizenet', 'Riskified_Full_Model_Authorizenet');
+    }
+
+    /**
+     * Make sure layout files are defined
+     *
+     * @test
+     */
+    public function testLayouts()
+    {
+        $this->assertLayoutFileDefined(
+            'frontend', 'full.xml'
+        );
+
+        $this->assertLayoutFileDefined(
+            'adminhtml', 'full.xml'
+        );
+    }
+
+    /**
+     * Make sure event observers related to status are properly defined
+     *
+     * @test
+     */
+    public function testStatusSyncEventObservers()
+    {
+        $this->assertEventObserverDefined(
+            'global', 'riskified_full_order_update', 'full/observer', 'updateOrderState'
+        );
+    }
+
+    /**
+     * Make sure event observers related to auto-invoicing are properly defined
+     *
+     * @test
+     */
+    public function testAutoInvoiceEventObservers()
+    {
+        $this->assertEventObserverDefined(
+            'global', 'riskified_full_order_update_approved', 'full/observer', 'autoInvoice'
+        );
+
+        $this->assertEventObserverDefined(
+            'global', 'riskified_full_order_update_captured', 'full/observer', 'autoInvoice'
+        );
+    }
+}
