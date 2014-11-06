c87e1d707a285fc73dd002268a38033807928b2d
dormin <nimrod@riskified.com>
2014-11-06 17:28:19 +0200

    skip all validations return meaningful response from Order Notifications

--- app/code/community/Riskified/Full/Helper/Order.php
+++ app/code/community/Riskified/Full/Helper/Order.php
@@ -118,7 +118,7 @@ class Riskified_Full_Helper_Order extends Mage_Core_Helper_Abstract {
      *      - riskified_order_update_error
 	 *      - riskified_order_update_?
 	 *
-	 * @param Mage_Sales_Model_Order $order
+	 * @esam Mage_Sales_Model_Order $order
 	 * @param string $status
 	 * @param string $description
 	 * @return void
@@ -173,7 +173,7 @@ class Riskified_Full_Helper_Order extends Mage_Core_Helper_Abstract {
         $sdkVersion = Riskified::VERSION;
 
 	    Mage::helper('full/log')->log("Riskified initSdk() - shop: $shopDomain, env: $env, token: $authToken, extension_version: $this->version, sdk_version: $sdkVersion");
-        Riskified::init($shopDomain, $authToken, $env, true);
+        Riskified::init($shopDomain, $authToken, $env, Validations::SKIP);
     }
 
     private function getHeaders() {
--- app/code/community/Riskified/Full/controllers/ResponseController.php
+++ app/code/community/Riskified/Full/controllers/ResponseController.php
@@ -5,19 +5,46 @@ class Riskified_Full_ResponseController extends Mage_Core_Controller_Front_Actio
     public function getresponseAction()
     {
         $request = $this->getRequest();
+        $response = $this->getResponse();
         $helper = Mage::helper('full/order');
-        $notification = $helper->parseRequest($request);
+        $logger = Mage::helper('full/log');
+        $statusCode = 200;
+        $id = null;
+        $msg = null;
 
-	    Mage::helper('full/log')->log("Notification received: " , serialize($notification));
+        try {
+            $notification = $helper->parseRequest($request);
+            $id = $notification->id;
 
-        $order = Mage::getModel('sales/order')->load($notification->id);
+            Mage::helper('full/log')->log("Notification received: ", serialize($notification));
 
-	    // Make sure we have a valid order to work with
-	    if (!$order || !$order->getId()) {
-		    Mage::helper('full/log')->log("ERROR: Unable to load order (" . $notification->id . ")");
-		    return;
-	    }
-        $helper->updateOrder($order, $notification->status, $notification->description);
+            $order = Mage::getModel('sales/order')->load($id);
+            if (!$order || !$order->getId()) {
+                $logger->log("ERROR: Unable to load order (" . $id . ")");
+                $statusCode = 400;
+                $msg = 'Could not find order to update.';
+            } else {
+                $helper->updateOrder($order, $notification->status, $notification->description);
+                $statusCode = 200;
+                $msg = 'Order-Update event triggered.';
+            }
+        } catch (Riskified\DecisionNotification\Exception\AuthorizationException $e) {
+            $logger->logException($e);
+            $statusCode = 401;
+            $msg = 'Authentication Failed.';
+        } catch (\Riskified\DecisionNotification\Exception\BadPostJsonException $e) {
+            $logger->logException($e);
+            $statusCode = 400;
+            $msg = "JSON Parsing Error.";
+        } catch (Exception $e) {
+            $logger->log("ERROR: while processing notification for order $id");
+            $statusCode = 500;
+            $msg = "Internal Error";
+        }
+
+        $response->setHttpResponseCode($statusCode);
+        $response->setHeader('Content-Type', 'application/json');
+        $response->setBody('{ "order" : { "id" : "' . $id . '", "description" : "' . $msg .'" } }');
+
     }
 }
-    
