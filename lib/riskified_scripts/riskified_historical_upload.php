fa5cf6aa2845575d30d02eaa83eac54caddbba56
Assaf Feldman <assaf@riskified.com>
2015-01-25 16:16:08 +0200

    use validations

--- lib/riskified_scripts/riskified_historical_upload.php
+++ lib/riskified_scripts/riskified_historical_upload.php
@@ -3,7 +3,7 @@
 //chdir('../magento/');
  
 set_include_path(dirname(__FILE__) . PATH_SEPARATOR . get_include_path());
-require '../../app/Mage.php';
+require 'app/Mage.php';
 
 umask (0);
 Mage::app(); // can set the run code/type, just like in the Mage::run() call
@@ -17,14 +17,15 @@ echo "Riskified auth token: $authToken \n";
 echo "Riskified shop domain: $domain \n";
 echo "Riskified target environment: $env \n";
 echo "*********** \n";
-include __DIR__ . '../riskified_php_sdk/src/Riskified/autoloader.php';
+include __DIR__ . 'lib/riskified_php_sdk/src/Riskified/autoloader.php';
 use Riskified\Common\Riskified;
 use Riskified\Common\Env;
+use Riskified\Common\Validations;
 use Riskified\Common\Signature;
 use Riskified\OrderWebhook\Model;
 use Riskified\OrderWebhook\Transport;
 
-Riskified::init($domain, $authToken, $env);
+Riskified::init($domain, $authToken, $env, Validations::SKIP);
 // add your own code below:
 $batch_size = 10;
 $page = 1;
