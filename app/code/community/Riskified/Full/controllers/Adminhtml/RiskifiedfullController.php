986dc5e63fb09fe06d39f11f94ffa9f63b094398
Nachshon Fishbane <nahshf@gmail.com>
2016-02-26 12:34:21 +0100

    Added new action in controller and button in system configuration

--- app/code/community/Riskified/Full/controllers/Adminhtml/RiskifiedfullController.php
+++ app/code/community/Riskified/Full/controllers/Adminhtml/RiskifiedfullController.php
@@ -29,4 +29,41 @@ class Riskified_Full_Adminhtml_RiskifiedfullController extends Mage_Adminhtml_Co
         $call->postOrderIds($order_ids);
         Mage::app()->getResponse()->setRedirect(Mage::helper('adminhtml')->getUrl("adminhtml/sales_order/index"));
     }
+
+    public function sendHistoricalOrdersAction() {
+        ignore_user_abort(true);
+        set_time_limit(0);
+
+        $batch_size = 10;
+        $page = intval(1);
+        if (!$page) {
+            $page = 1;
+        }
+        $orders = Mage::getModel('sales/order')->getCollection();
+
+        $total_count = $orders->getSize();
+
+        $orders_collection = Mage::getModel('sales/order')
+            ->getCollection()
+            ->setPageSize($batch_size)
+            ->setCurPage($page);
+
+        $total_uploaded = 0;
+
+        while ($total_uploaded < $total_count) {
+            try {
+                Mage::helper('full/order')->postHistoricalOrders($orders_collection);
+                $total_uploaded += $orders_collection->count();
+                $page++;
+                $orders_collection = Mage::getModel('sales/order')
+                    ->getCollection()
+                    ->setPageSize($batch_size)
+                    ->setCurPage($page);
+                Mage::log($total_uploaded, null, 'riskified_full.log');
+            } catch (Exception $e) {
+                Mage::logException($e);
+                exit(1);
+            }
+        }
+    }
 }
