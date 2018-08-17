8fdf294d99f3645c519d996e8344ce867505e89c
nfishbane <nahshf@gmail.com>
2018-08-17 10:04:24 +0200

    set card bin for pagarme payment

--- app/code/community/Riskified/Full/Helper/Order/Payment.php
+++ app/code/community/Riskified/Full/Helper/Order/Payment.php
@@ -224,6 +224,9 @@ class Riskified_Full_Helper_Order_Payment extends Mage_Core_Helper_Abstract
                     $creditCardCompany = $payment->getCcType();
                     $transactionId = $payment->getPagarmeTransactionId();
                     $avsResultCode = $payment->getPagarmeAntifraudScore();
+                    if ($payment->getCardFirstDigits()) {
+                        $creditCardBin = $payment->getCardFirstDigits();
+                    }
                     break;
 
                 default:
