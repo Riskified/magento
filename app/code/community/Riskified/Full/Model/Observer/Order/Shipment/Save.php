559637f716ea33e3aa4a5948877b03374cf7546e
Wojciech Kamiski <wojtek.kaminski@gmail.com>
2019-01-24 11:18:51 +0100

    Added validation if shipment was already processed

--- app/code/community/Riskified/Full/Model/Observer/Order/Shipment/Save.php
+++ app/code/community/Riskified/Full/Model/Observer/Order/Shipment/Save.php
@@ -2,10 +2,17 @@
 
 class Riskified_Full_Model_Observer_Order_Shipment_Save
 {
+    const REGISTRY_INDEX_NAME = 'riskified-processed-shipments';
+
     public function handleShipmentSave(
         Varien_Event_Observer $observer
     ) {
         $shipment = $observer->getEvent()->getShipment();
+
+        if (!$this->validate($shipment->getId())) {
+            Mage::helper('full/log')->log('Shipment was already sent to the API in this application call. Stopping process.');
+            return $this;
+        }
 
         $order = $shipment->getOrder();
         $helper = Mage::helper('full/order');
@@ -18,5 +25,38 @@ class Riskified_Full_Model_Observer_Order_Shipment_Save
             Mage::getSingleton('adminhtml/session')->addError('Riskified shipment error save: ' . $e->getMessage());
             Mage::helper('full/log')->logException($e);
         }
+    }
+
+    /**
+     * Method validates if the shipment can be processed.
+     * It prevents to send multiple fulfill call during shipment save.
+     *
+     * @param int $shipmentId Shipment ID created by magento backend.
+     * @return bool
+     * @throws Mage_Core_Exception
+     */
+    protected function validate($shipmentId)
+    {
+        $registeredShipments = Mage::registry(self::REGISTRY_INDEX_NAME);
+
+        if (!is_array($registeredShipments)) {
+            $registeredShipments = array();
+            $registeredShipments[] = $shipmentId;
+
+            Mage::unregister(self::REGISTRY_INDEX_NAME);
+            Mage::register(self::REGISTRY_INDEX_NAME, $registeredShipments);
+
+            return true;
+        }
+
+        if (!in_array($shipmentId, $registeredShipments)) {
+            $registeredShipments[] = $shipmentId;
+            Mage::unregister(self::REGISTRY_INDEX_NAME);
+            Mage::register(self::REGISTRY_INDEX_NAME, $registeredShipments);
+
+            return true;
+        }
+
+        return false;
     }
 }
